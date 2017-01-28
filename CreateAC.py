d719047a96ddf4a77ed1508ed2b42867634446f8
joanibal <joanibal@umich.edu>
2017-01-28 15:32:29 -0500

    added DV

--- CreateAC.py
+++ CreateAC.py
@@ -15,55 +15,71 @@ import matplotlib.pyplot as plt
 from time import localtime, strftime, time
 
 
-from xfoil.xfoil_lib import xfoil_alt, getData_xfoil
-from lib_aero import get_aeroCoef, num_laps
+
+from Input_Files.Input import AC
 
 
-
-
-class AeroAnalysis(Component):
+class createAC(Component):
 	'''calculate score'''
 	# set up interface to the framework
 	def __init__(self ):
-		super(obj,self).__init__()
+		super(createAC,self).__init__()
 
-		self.add_param('aircraft',val=aircraft(),desc='Wing Span [m]')
+		self.add_param('def_aircraft',val=AC, desc='Aircraft Class')
+		self.add_param('span',val=0.0, desc='wing span')
 
 
 		# # set up outputs
 
-		self.add_output('score', val= 0.0,desc='score ')
-		self.add_output('N', val = 0.0, desc = 'number of laps')
-		self.add_output('SM', val = 0.0, desc = 'static margin')
-		self.add_output('NP', val = 0.0, desc = 'Netual point')
-		self.add_output('tot_time', val = 0.0, desc = 'time')
+		self.add_output('aircraft', val=AC,desc='score ')
+		# self.add_output('N', val = 0.0, desc = 'number of laps')
+		# self.add_output('SM', val = 0.0, desc = 'static margin')
+		# self.add_output('NP', val = 0.0, desc = 'Netual point')
+		# self.add_output('tot_time', val = 0.0, desc = 'time')
 
 	def solve_nonlinear(self,params,unknowns,resids):
 		# make all input variables local for ease
 		# C = [params['C1'], params['C2'], params['C3'], params['C4'], params['C5']]
-		b_wing = params['b_wing']
-		Sref_wing = params['Sref_wing']
-		Sref_tail = params['Sref_tail']
+		AC = params['def_aircraft']
+	
+		# print AC.
 
 		# print(params['mass'])
+		AC.wing.b_wing = params['span']
+
+
+		unknowns['aircraft'] = AC
+
+		# # unknowns['SM'] = 0.0
+		# unknowns['N'] = 0
+		# unknowns['score'] = 0.0
+		# unknowns['tot_time'] = 300
+		# unknowns['NP'] = MAC/4.0
 
 
 
 
-		# unknowns['SM'] = 0.0
-		unknowns['N'] = 0
-		unknowns['score'] = 0.0
-		unknowns['tot_time'] = 300
-		unknowns['NP'] = MAC/4.0
-
-
-
-
-		print('\n')
-		print('============== output =================')
-		print('N: ' + str(unknowns['N']))	
-		print('SM: ' + str(unknowns['SM']))
-		print('Score: ' + str( unknowns['score']))
-		print('\n')
+		# print('\n')
+		# print('============== output =================')
+		# print('N: ' + str(unknowns['N']))	
+		# print('SM: ' + str(unknowns['SM']))
+		# print('Score: ' + str( unknowns['score']))
+		# print('\n')
 
 			# print('==============================================')
+
+
+root = Group()
+root.add('indep_var', IndepVarComp('span', 7.0))
+root.add('my_comp', createAC())
+root.connect('indep_var.span', 'my_comp.span')
+
+prob = Problem(root)
+prob.setup()
+prob.run()
+
+out_ac = prob['my_comp.aircraft']
+print out_ac.wing.b_wing
+# count = prob.root.my_comp.counter
+# print(result)
+# print(count)
