fixed rotation angle

MDO_Top.py
@@ -42,13 +42,13 @@ class constrainedMDO(Group):
 		self.add('sweep',IndepVarComp('sweep', np.array([0.0, 0.0, 0.0, 0.0])))# Quarter Chord Sweep in degrees (cubic)
 		self.add('chord',IndepVarComp('chord',np.array([0.0, 0.0, 0.0, 0.08])))	# Chord (cubic constants: chord = ax^3+bx^2+c*x+d, x = half-span position)		
 		# self.add('dist_LG',IndepVarComp('dist_LG', 1.0))						# Distance between CG and landing gear (m)
-		self.add('boom_len',IndepVarComp('boom_len', 0.75))						# Length of tailboom (m)
+		self.add('boom_len',IndepVarComp('boom_len', 1.0))						# Length of tailboom (m)
 		# self.add('camber',IndepVarComp('camber',np.array([1.0 , 1.0, 1.0,1.0])))# Wing camber (cubic constants: camber = c_ax^3+c_bx^2+c_c*x + c_d, x = half-span position)
 		# self.add('max_camber',IndepVarComp('max_camber',np.array([1.0 , 1.0, 1.0,1.0])))		# Horizontail Tail Span 
 		# self.add('thickness',IndepVarComp('thickness',np.array([1.0 , 1.0, 1.0,1.0])))		# Tail Root airfoil Cord 
 		# self.add('max_thickness',IndepVarComp('max_thickness',np.array([1.0 , 1.0, 1.0,1.0])))	# Vertical Tail Span
 		# self.add('Ainc',IndepVarComp('Ainc',np.array([1.0 , 1.0, 1.0,1.0])))	# Boom Length
-		self.add('htail_chord',IndepVarComp('htail_chord',np.array([0.0 , 0.0, 0.0,0.04]))) # Horiz. Tail Chord (cubic constants: chord = ax^3+bx^2+c*x+d, x = half-span position)		
+		self.add('htail_chord',IndepVarComp('htail_chord',np.array([0.0 , 0.0, 0.0,0.1]))) # Horiz. Tail Chord (cubic constants: chord = ax^3+bx^2+c*x+d, x = half-span position)		
 		# self.add('c_r_vt',IndepVarComp('c_r_vt',np.array([0.0 , 0.0, 0.0,1.0])))
 		self.add('b_htail',IndepVarComp('b_htail',0.2))
 		self.add('b_vtail',IndepVarComp('b_vtail',0.3))
@@ -127,7 +127,7 @@ prob.root = constrainedMDO()
 
 prob.driver = ScipyOptimizer()
 prob.driver.options['optimizer'] = 'SLSQP'
-prob.driver.options['tol'] = 1.0e-2
+prob.driver.options['tol'] = 1.0e-4
 prob.root.fd_options['force_fd'] = True	
 prob.root.fd_options['form'] = 'central'
 prob.root.fd_options['step_size'] = 1.0e-6
@@ -173,7 +173,7 @@ num_sections = 5
 prob.driver.add_objective('objPerformance.score')
 # prob.driver.add_constraint('objPerformance.sum_y', lower = 0.0)
 prob.driver.add_constraint('objPerformance.chord_vals', lower = np.ones((num_sections,1))*0.001  )
-prob.driver.add_constraint('objPerformance.htail_chord_vals', lower = np.ones((num_sections,1))*0.001  )
+prob.driver.add_constraint('objPerformance.htail_chord_vals', lower = np.ones((num_sections,1))*0.01  )
 # prob.driver.add_constraint('aeroAnalysis.SM', lower = 0.05, upper = 0.4)
 prob.driver.add_constraint('structAnalysis.stress_wing', lower = 0.00, upper = 60000)
 prob.driver.add_constraint('structAnalysis.stress_tail', lower = 0.00, upper = 60000)

Performance/objPerformance.py
@@ -262,7 +262,6 @@ def calcCruise_time(dist, velocity):
 
 def calcTurn_time(bank_angle, velocity, weight ,Sref_wing, turn_angle):
 	velocity = velocity/2
-
 	bank_angle_r = bank_angle*np.pi/180
 	g = 9.81
 
@@ -275,8 +274,18 @@ def calcTurn_time(bank_angle, velocity, weight ,Sref_wing, turn_angle):
 
 def runwaySim_small(CL, CD, CM, sref_wing, sref_tail, weight, boom_len, dist_LG, MAC, Iyy):
 
+
 	Flapped = 0
-	max_rot_ang = 10*np.pi/180.0
+
+
+
+	landingGearHeight = 0.1 # [m]
+	# print (landingGearHeight - dist_LG)
+	# print landingGearHeight
+	max_rot_ang = np.arctan( 0.1/(boom_len - dist_LG)) 
+	# print max_rot_ang*180/np.pi
+	# exit()
+	# max_rot_ang = 10*np.pi/180.0
 
 
 	#declare functions for kinimatic varibles (F = ma and M = I*ang_a)
@@ -407,7 +416,6 @@ def runwaySim_small(CL, CD, CM, sref_wing, sref_tail, weight, boom_len, dist_LG,
 	# 400 ft runway length in meters
 	runway_len = 137.8
 
-	# print('from sim', sum_y)
 
 
 	# if (sum_y > 0.0 and dist[i] <= runway_len):
@@ -418,40 +426,40 @@ def runwaySim_small(CL, CD, CM, sref_wing, sref_tail, weight, boom_len, dist_LG,
 	# ============== Ploting ===============
 
 	# plt.figure(1)
-	# plt.subplot(711)
+	# plt.subplot(611)
 	# plt.ylabel('Angle)')
 	# plt.xlabel('time')
 	# plt.plot(time, ang, 'b')
 
-	# plt.subplot(712)
+	# plt.subplot(612)
 	# plt.ylabel('ang velocity')
 	# plt.xlabel('time')
 	# plt.plot(time, ang_vel, 'b')
 
-	# plt.subplot(713)
+	# plt.subplot(613)
 	# plt.ylabel('ang acceleration')
 	# plt.xlabel('time')
 	# plt.plot(time, ang_accel, 'b')
 
-	# plt.subplot(714)
+	# plt.subplot(614)
 	# plt.ylabel('distance')
 	# plt.xlabel('time')
 	# plt.plot(time, dist, 'b')
 
-	# plt.subplot(715)
+	# plt.subplot(615)
 	# plt.ylabel('Velocity')
 	# plt.xlabel('time')
 	# plt.plot(time, vel, 'b')
 
-	# plt.subplot(716)
+	# plt.subplot(616)
 	# plt.ylabel('Acceleration')
 	# plt.xlabel('time')
 	# plt.plot(time, accel, 'b')
 
-	# plt.subplot(717)
-	# plt.ylabel('dt')
-	# plt.xlabel('time')
-	# plt.plot(time, DT, 'b')
+	# # plt.subplot(717)
+	# # plt.ylabel('dt')
+	# # plt.xlabel('time')
+	# # plt.plot(time, DT, 'b')
 
 	# print('weight: ' + str(weight))
 	# print('Sum Y:' + str(sum_y))