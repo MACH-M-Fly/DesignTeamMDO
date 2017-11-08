4c4f222bba6cc8f9eacc508f68a5a8efe8d47984
Ian O'Rourke <ianor@umich.edu>
2017-11-07 19:25:43 -0500

    Added supra

--- TakeoffDistanceExample.py
+++ TakeoffDistanceExample.py
@@ -19,7 +19,19 @@ dist_LG = 0.12827
 MAC = 0.496824
 Iyy = 0.3350
 
-# Typical Micro-Class Aircraft
+# Mark Drela's Supra
+geo_filename = 'Aerodynamics/aircraft_supra.txt'
+mass_filename = 'Aerodynamics/aircraft_supra.mass'
+
+alpha, CL, CD, CM, NP, sec_CL, sec_Yle, sec_Chord, velocity = aeroAnalysis.getAeroCoef(geo_filename=geo_filename, mass_filename=mass_filename)
+
+sref_wing = 0.6787083
+sref_tail = 0.0535483
+weight = 1.81437 * 9.81
+boom_len = 1.0668
+dist_LG = 0.20193
+MAC = 0.20193
+Iyy = 0.1008
 
 print('Running Takeoff Sim')
 
